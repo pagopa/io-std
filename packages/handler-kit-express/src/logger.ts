62e878cb0625811170adfb70dfbc81c370d77956
Silvia Cirulli <silviacirulli@MacBook-Pro-di-Silvia.local>
2023-04-13 14:08:10 +0200

    feat: add logger implementation using Express.Request.log from @pagopa/logger-express

--- packages/handler-kit-express/src/logger.ts
+++ packages/handler-kit-express/src/logger.ts
@@ -1,19 +1,22 @@
 /* eslint-disable no-console */
 import * as L from "@pagopa/logger";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import * as LE from "@pagopa/logger-express";
 
-// Derive a concrete implementation L.Logger using console
-export const getLogger = (): L.Logger => ({
-  log: (s, level) => () => {
-    const logFunc: Record<typeof level, (s: string) => void> = {
-      debug: console.debug,
-      info: console.info,
-      warn: console.warn,
-      error: console.error,
-      fatal: console.error,
-    };
-    logFunc[level](s);
-  },
-  format:
-    process.env.NODE_ENV === "development" ? L.format.simple : L.format.json,
-  context: {},
-});
+// Derive a concrete implementation L.Logger using Express.Request.log, if present, otherwise using console
+export const getLogger = (req: Express.Request): L.Logger =>
+  req.logger ?? {
+    log: (s, level) => () => {
+      const logFunc: Record<typeof level, (s: string) => void> = {
+        debug: console.debug,
+        info: console.info,
+        warn: console.warn,
+        error: console.error,
+        fatal: console.error,
+      };
+      logFunc[level](s);
+    },
+    format:
+      process.env.NODE_ENV === "development" ? L.format.simple : L.format.json,
+    context: {},
+  };
