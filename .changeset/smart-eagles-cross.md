6550d4cd0a789b882ece6f0eadf2270e7f0d911c
Luca Cavallaro <luca.cavallaro@pagopa.it>
2023-04-17 14:54:13 +0200

    fix(azure-func): return binding metadata as input on blob-trigger
    Reviewed-by: @gunzip 
    Refs: #20

---
+++ .changeset/smart-eagles-cross.md
@@ -0,0 +1,5 @@
+---
+"@pagopa/handler-kit-azure-func": patch
+---
+
+Now functions triggered by blobTrigger returns their metadata instead of the wrong payload
--- packages/handler-kit-azure-func/src/trigger.ts
+++ packages/handler-kit-azure-func/src/trigger.ts
@@ -24,6 +24,8 @@ const FunctionTrigger = t.type({
   name: t.string,
 });
 
+type FunctionTrigger = t.TypeOf<typeof FunctionTrigger>;
+
 const getFunctionTrigger = () =>
   flow(
     (ctx: azure.Context) => ctx.bindingDefinitions,
@@ -34,23 +36,26 @@ const getFunctionTrigger = () =>
 export class BindingNotFoundError extends Error {
   name = "BindingNotFoundError";
   bindingName: string;
-  bindings: azure.ContextBindingData;
-  constructor(bindingName: string, bindings: azure.ContextBindingData) {
+  bindings: azure.ContextBindings;
+  constructor(bindingName: string, bindings: azure.ContextBindings) {
     super("Unable to find binding data");
     this.bindingName = bindingName;
     this.bindings = bindings;
   }
 }
 
-const getBindingData = (name: string) => (ctx: azure.Context) =>
-  pipe(
+const getBindings = (t: FunctionTrigger) => (ctx: azure.Context) => {
+  if (t.type === "blobTrigger") {
+    return E.right(ctx.bindingData);
+  }
+  return pipe(
     ctx.bindings,
-    lookup(name),
-    E.fromOption((): Error => new BindingNotFoundError(name, ctx.bindingData))
+    lookup(t.name),
+    E.fromOption((): Error => new BindingNotFoundError(t.name, ctx.bindings))
   );
+};
 
 export const getTriggerBindingData = flow(
   getFunctionTrigger,
-  RE.map((trigger) => trigger.name),
-  RE.chain(getBindingData)
+  RE.chain(getBindings)
 );
