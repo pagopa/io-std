9aaf7af3a1cfd72733e1b6bfbab66f2a1b269392
silvicir <127971996+silvicir@users.noreply.github.com>
2023-11-06 12:07:07 +0100

    fix: updated get logger function
    Reviewed-by: @lucacavallaro 
    Refs: #34

---
+++ .changeset/pretty-jobs-sing.md
@@ -0,0 +1,5 @@
+---
+"@pagopa/handler-kit-azure-func": patch
+---
+
+Updated getLogger function
--- packages/handler-kit-azure-func/src/logger.ts
+++ packages/handler-kit-azure-func/src/logger.ts
@@ -4,15 +4,9 @@ import * as L from "@pagopa/logger";
 
 // Derive a concrete implementation L.Logger using azure.Context.log
 export const getLogger = (ctx: InvocationContext): L.Logger => ({
-  log: (s, level) => () => {
-    const logFunc: Record<typeof level, (s: string) => void> = {
-      debug: ctx.debug,
-      info: ctx.info,
-      warn: ctx.warn,
-      error: ctx.error,
-      fatal: ctx.error,
-    };
-    logFunc[level](s);
+  log: (s, _level) => () => {
+    const level = _level === "fatal" ? "error" : _level;
+    ctx[level](s);
   },
   format:
     process.env.NODE_ENV === "development" ? L.format.simple : L.format.json,
